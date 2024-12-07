721daceb5166bbffe21f4600b26724c270b9bc90
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-12-07 11:25:53 +0800

    Revert "WIP: move to OffscreenCanvas"
    This reverts commit 5b4a9cf6634b0bb7d9cf344b897dcad320f14bf7.

--- .eslintrc.js
+++ .eslintrc.js
@@ -53,7 +53,6 @@ module.exports = {
     '@typescript-eslint/prefer-for-of': 'off',
     '@typescript-eslint/restrict-template-expressions': 'off',
     '@typescript-eslint/array-type': ['error', { default: 'generic' }],
-    'prefer-object-spread': 'off',
   },
   settings: {
     'import/resolver': {
